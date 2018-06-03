3b251442b1dbb6dc27fb70825f3cc24b67ee2fe8
Daniel Persson <mailto.woden@gmail.com>
2018-06-03 21:34:39 +0200

    Updated to build a more precise model.

--- README.md
+++ README.md
@@ -29,3 +29,37 @@ python train.py
 ```
 
 If you have any question or suggestion the just reach out. Open an issue and I'll look into it.
+
+
+## Tensorflow lite model.
+
+In order to use this model on a tensorflow lite enabled device you need to freeze your model using this command
+
+```
+freeze_graph \
+  --input_graph=./model2/graph.pbtxt \
+  --input_checkpoint=./model2/model.ckpt-81852 \
+  --input_binary=false \
+  --output_graph=/tmp/frozen.pb \
+  --output_node_names=Softmax
+```
+
+In order to convert your model you need a tool called toco (Tensorflow Lite Optimizing Converter). Use the command below to build this tool in your tensorflow directory.
+```
+bazel build //tensorflow/contrib/lite/toco:toco
+```
+
+After that you convert it into a tensorflow lite model using the command below inside of your tensorflow directory.
+
+```
+ ./bazel-bin/tensorflow/contrib/lite/toco/toco 
+   --input_file=/tmp/frozen.pb 
+   --input_format=TENSORFLOW_GRAPHDEF 
+   --output_format=TFLITE 
+   --output_file=/tmp/cat_vs_dogs.tflite 
+   --input_arrays=Reshape 
+   --output_arrays=Softmax 
+   --input_shapes=1,224,224,3
+```
+
+Modifying the demo provided by google you can then test your inference on your device.
--- train.py
+++ train.py
@@ -21,11 +21,16 @@ def parser(record):
 
 
 def input_fn(filenames):
-  dataset = tf.data.TFRecordDataset(filenames=filenames)
-  dataset = dataset.shuffle(buffer_size=2048)
-  dataset = dataset.map(map_func=parser)
-  dataset = dataset.batch(batch_size=32)
-  dataset = dataset.prefetch(buffer_size=2048)
+  dataset = tf.data.TFRecordDataset(filenames=filenames, num_parallel_reads=40)
+  dataset = dataset.apply(
+      tf.contrib.data.shuffle_and_repeat(1024, 1)
+  )
+  dataset = dataset.apply(
+      tf.contrib.data.map_and_batch(parser, 32)
+  )
+  #dataset = dataset.map(parser, num_parallel_calls=12)
+  #dataset = dataset.batch(batch_size=1000)
+  dataset = dataset.prefetch(buffer_size=2)
   return dataset
 
 
@@ -36,10 +41,14 @@ def val_input_fn():
     return input_fn(filenames=["val.tfrecords"])
 
 def model_fn(features, labels, mode, params):
-    num_classes = 2
+    num_classes = 3
     net = features["image"]
 
+    net = tf.identity(net, name="input_tensor")
+    
     net = tf.reshape(net, [-1, 224, 224, 3])    
+
+    net = tf.identity(net, name="input_tensor_after")
 
     net = tf.layers.conv2d(inputs=net, name='layer_conv1',
                            filters=32, kernel_size=3,
@@ -47,20 +56,35 @@ def model_fn(features, labels, mode, params):
     net = tf.layers.max_pooling2d(inputs=net, pool_size=2, strides=2)
 
     net = tf.layers.conv2d(inputs=net, name='layer_conv2',
-                           filters=32, kernel_size=3,
+                           filters=64, kernel_size=3,
+                           padding='same', activation=tf.nn.relu)
+    net = tf.layers.max_pooling2d(inputs=net, pool_size=2, strides=2)  
+
+    net = tf.layers.conv2d(inputs=net, name='layer_conv3',
+                           filters=64, kernel_size=3,
                            padding='same', activation=tf.nn.relu)
     net = tf.layers.max_pooling2d(inputs=net, pool_size=2, strides=2)    
 
     net = tf.contrib.layers.flatten(net)
 
     net = tf.layers.dense(inputs=net, name='layer_fc1',
-                          units=128, activation=tf.nn.relu)    
+                        units=128, activation=tf.nn.relu)  
+    
+    net = tf.layers.dropout(net, rate=0.5, noise_shape=None, 
+                        seed=None, training=(mode == tf.estimator.ModeKeys.TRAIN))
+    
     net = tf.layers.dense(inputs=net, name='layer_fc_2',
-                          units=num_classes)
+                        units=num_classes)
 
     logits = net
     y_pred = tf.nn.softmax(logits=logits)
+
+    y_pred = tf.identity(y_pred, name="output_pred")
+
     y_pred_cls = tf.argmax(y_pred, axis=1)
+
+    y_pred_cls = tf.identity(y_pred_cls, name="output_cls")
+
 
     if mode == tf.estimator.ModeKeys.PREDICT:
         spec = tf.estimator.EstimatorSpec(mode=mode,
@@ -87,10 +111,10 @@ def model_fn(features, labels, mode, params):
 
 model = tf.estimator.Estimator(model_fn=model_fn,
                                params={"learning_rate": 1e-4},
-                               model_dir="./model2/")
+                               model_dir="./model5/")
 
 count = 0
-while (count < 100):
+while (count < 100000):
     model.train(input_fn=train_input_fn, steps=1000)
     result = model.evaluate(input_fn=val_input_fn)
     print(result)
